17dd40f6565d8fc7ff2a5478f8eaa63833f21403
jackson211 <jackson21182@gmail.com>
2019-09-08 01:49:13 +1000

    update readme

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # dotfile
+
+## Config file
+run ``` ./install_zsh.sh``` to install oh\_my\_zsh config
+
+run ``` ./install_vim.sh``` to install vim config
+
+run ``` ./install_brew.sh``` to install homwbrew Formula
--- install_vim.sh
+++ install_vim.sh
@@ -3,6 +3,12 @@ cp -r ./.vim ~
 cp ./.vimrc ~
 
 # setup vundle
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+if git clone --depth=1 https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim;
+then
+    echo "pulling Vundle"
+else
+    cd ~/.vim/bundle/Vundle.vim && git pull
+fi
+
 
 vim +PluginInstall +qall
